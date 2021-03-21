7e0c7acc57d9896780a3a275e5969beaa6a863d9
Huzaifa <iib.tech357@gmail.com>
2021-03-21 10:00:20 -0700

    new commit

---
+++ lib/Components/colorConstants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+//
+// const color1 = Color(0xFF222831);
+// const color2 = Color(0xFF393e46);
+// const color3 = Color(0xFFb55400);
+// const color4 = Color(0xFFeeeeee);
+//
+// const color1 = Color(0xFF1b3c59);
+// const color2 = Color(0xFF456173);
+// const color3 = Color(0xFFa6ed8e);
+// const color4 = Color(0xFFf2f2f0);
+//
+const color1 = Color(0xFF41444b);
+const color2 = Color(0xFF52575d);
+const color3 = Color(0xFFc7a504);
+const color4 = Color(0xFFf6f4e6);
+const color5 = Color(0xFFdbc459);
+
+// const color1 = Color(0xFF222831);
+// const color2 = Color(0xFF393e46);
+// const color3 = Color(0xFFfd7014);
+// const color4 = Color(0xFFeeeeee);
+// const color5 = Color(0xFFf7a46d);
+const color6 = Color(0xFFefefef);
